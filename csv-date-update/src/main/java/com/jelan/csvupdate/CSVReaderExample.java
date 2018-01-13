321fea9d5c828866ccef174ba5213571ea8cfb4c
nirmalyakar <nirmalyakar00@gmail.com>
2018-01-14 00:42:33 +0530

    first approach 10 %

--- csv-date-update/src/main/java/com/jelan/csvupdate/CSVReaderExample.java
+++ csv-date-update/src/main/java/com/jelan/csvupdate/CSVReaderExample.java
@@ -1,7 +1,5 @@
 package com.jelan.csvupdate;
 
-import com.opencsv.CSVReader;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,15 +8,58 @@ import java.util.Map;
 
 import org.apache.commons.collections.map.HashedMap;
 
+import com.opencsv.CSVReader;
+
 public class CSVReaderExample {
-
-    public static void main(String[] args) {
-
+	public CSVReaderExample(String workinglocation) {
+		super();
+		this.WORKINGLOCATION=workinglocation;
+	}
+	private final String WORKINGLOCATION;
+	
+	private void processCSVWithThisOrder(Map<String, List<String>> thingsToProcess) {
+		for (Map.Entry<String, List<String>> entry : thingsToProcess.entrySet()) {
+			String currentCsvFileToProcess=WORKINGLOCATION+"\\"+entry.getKey()+".csv";
+			CSVReader csvReader;
+			try {
+				csvReader=new CSVReader(new FileReader(currentCsvFileToProcess));
+				String[] heading=csvReader.readNext();
+				Map<String, Integer> columnNameToIndexMap=createColumnNameToIndexMapFormTheseHeadings(heading);
+				String[] line;
+				while((line=csvReader.readNext())!=null){
+					for(int i=0;i<entry.getValue().size();i++){
+						System.out.println("---"+entry.getValue().get(i));
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println(entry.getKey());
+			for(int i=0;i<entry.getValue().size();i++){
+				System.out.println("---"+entry.getValue().get(i));
+			}
+		}
+		
+	}
+    private Map<String, Integer> createColumnNameToIndexMapFormTheseHeadings(String[] heading) {
+    	Map<String, Integer> columnNameToIndexMap=new HashedMap();
+    	for (int i=0;i<heading.length;i++) {
+			columnNameToIndexMap.put(heading[i], i);
+		}
+		return columnNameToIndexMap;
+	}
+	public static void main(String[] args) {
+    	CSVReaderExample csvReaderExample=new CSVReaderExample(args[0]);
+    	try{
+    		csvReaderExample=new CSVReaderExample(args[0]);
+    	}catch (Exception e) {
+    		e.printStackTrace();
+		}
         String csvFile = "C:\\Users\\Nirmalya\\Desktop\\DBAdminRequirement\\config.csv";
         Map<String, List<String>> thingsToProcess=new HashedMap();  
         CSVReader reader = null;
         try {
-            reader = new CSVReader(new FileReader(csvFile),',','\'',1 );
+            reader = new CSVReader(new FileReader(csvFile),',','\'',1);
             String[] line;
             while ((line = reader.readNext()) != null) {
             	List<String> columns;
@@ -32,22 +73,11 @@ public class CSVReaderExample {
             		}
             	}
             }
-            processCSVWithThisOrder(thingsToProcess);
+            csvReaderExample.processCSVWithThisOrder(thingsToProcess);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
     }
-
-	private static void processCSVWithThisOrder(Map<String, List<String>> thingsToProcess) {
-		for (Map.Entry<String, List<String>> entry : thingsToProcess.entrySet()) {
-			System.out.println(entry.getKey());
-			for(int i=0;i<entry.getValue().size();i++){
-				System.out.println("---"+entry.getValue().get(i));
-			}
-		}
-		
-	}
-
 }
