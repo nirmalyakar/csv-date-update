1d9a65b795f8d822ee24d85f75cf8d0b2f3dcc7d
nirmalyakar <nirmalyakar00@gmail.com>
2018-01-14 15:26:44 +0530

    updating the files successfully

--- csv-date-update/src/main/java/com/jelan/csvupdate/CSVReaderExample.java
+++ csv-date-update/src/main/java/com/jelan/csvupdate/CSVReaderExample.java
@@ -3,7 +3,15 @@ package com.jelan.csvupdate;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -13,29 +21,30 @@ import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 
 public class CSVReaderExample {
-	public CSVReaderExample(String workinglocation) {
-		super();
-		this.WORKINGLOCATION=workinglocation;
-	}
 	private final String WORKINGLOCATION;
-	
-	private void processCSVWithThisOrder(Map<String, List<String>> thingsToProcess) {
+	private final int DAYSTOINCREASE;
+	public CSVReaderExample(String workinglocation,int daysToIncrease) {
+		super();
+		this.WORKINGLOCATION = workinglocation;
+		this.DAYSTOINCREASE = daysToIncrease;
+	}
+
+	private void processCSVWithThisOrder(Map<String, List<String>> thingsToProcess) throws ParseException {
 		for (Map.Entry<String, List<String>> entry : thingsToProcess.entrySet()) {
-			String currentCsvFileToProcess=WORKINGLOCATION+"\\"+entry.getKey()+".csv";
+			String currentCsvFileToProcess = WORKINGLOCATION + "\\" + entry.getKey() + ".csv";
 			CSVReader csvReader;
 			try {
-				csvReader=new CSVReader(new FileReader(currentCsvFileToProcess));
-				List<String[]> csvBody=csvReader.readAll();
-				String[] heading=csvBody.get(0);//Expecting first line as heading
-				Map<String, Integer> columnNameToIndexMap=createColumnNameToIndexMapFormTheseHeadings(heading);
-				for(int i=1;i<csvBody.size();i++){
+				csvReader = new CSVReader(new FileReader(currentCsvFileToProcess));
+				List<String[]> csvBody = csvReader.readAll();
+				String[] heading = csvBody.get(0);// Expecting first line as heading
+				Map<String, Integer> columnNameToIndexMap = createColumnNameToIndexMapFormTheseHeadings(heading);
+				for (int i = 1; i < csvBody.size(); i++) {
 					for (int columnNameCounter = 0; columnNameCounter < entry.getValue().size(); columnNameCounter++) {
-						//System.out.println(csvBody.get(i)[columnNameToIndexMap.get("File 2 Field4")]);
-						csvBody.get(i)[columnNameToIndexMap.get(entry.getValue().get(columnNameCounter))]="changedText";
+						csvBody.get(i)[columnNameToIndexMap.get(entry.getValue().get(columnNameCounter))] = datetoUpdate(csvBody.get(i)[columnNameToIndexMap.get(entry.getValue().get(columnNameCounter))]);
 					}
 				}
 				csvReader.close();
-				CSVWriter csvWriter=new CSVWriter(new FileWriter(currentCsvFileToProcess));
+				CSVWriter csvWriter = new CSVWriter(new FileWriter(currentCsvFileToProcess));
 				csvWriter.writeAll(csvBody);
 				csvWriter.flush();
 				csvWriter.close();
@@ -43,45 +52,61 @@ public class CSVReaderExample {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
-    private Map<String, Integer> createColumnNameToIndexMapFormTheseHeadings(String[] heading) {
-    	Map<String, Integer> columnNameToIndexMap=new HashedMap();
-    	for (int i=0;i<heading.length;i++) {
+
+	private String datetoUpdate(String olddateinString) throws ParseException {
+		DateFormat dateFormat=new SimpleDateFormat("MM/dd/yyyy");
+		Date olddate=dateFormat.parse(olddateinString);
+		Calendar c =Calendar.getInstance();
+		c.setTime(olddate);
+		c.add(Calendar.DATE,DAYSTOINCREASE);
+		Date newDate=c.getTime();
+		System.out.println(newDate);
+		String newDateInString=dateFormat.format(newDate);
+		return newDateInString;
+	}
+
+
+	private Map<String, Integer> createColumnNameToIndexMapFormTheseHeadings(String[] heading) {
+		Map<String, Integer> columnNameToIndexMap = new HashedMap();
+		for (int i = 0; i < heading.length; i++) {
 			columnNameToIndexMap.put(heading[i], i);
 		}
 		return columnNameToIndexMap;
 	}
+
 	public static void main(String[] args) {
-    	CSVReaderExample csvReaderExample=new CSVReaderExample(args[0]);
-    	try{
-    		csvReaderExample=new CSVReaderExample(args[0]);
-    	}catch (Exception e) {
-    		e.printStackTrace();
+		CSVReaderExample csvReaderExample = null;
+		try {
+			csvReaderExample = new CSVReaderExample(args[0],Integer.parseInt(args[1]));
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-        String csvFile = csvReaderExample.WORKINGLOCATION+"\\config.csv";
-        Map<String, List<String>> thingsToProcess=new HashedMap();  
-        CSVReader reader = null;
-        try {
-            reader = new CSVReader(new FileReader(csvFile),',','\'',1);
-            String[] line;
-            while ((line = reader.readNext()) != null) {
-            	List<String> columns;
-            	if(line[4].toLowerCase().equalsIgnoreCase("yes")){
-            		if(thingsToProcess.containsKey(line[0])){
-            			thingsToProcess.get(line[0]).add(line[1]);
-            		}else{
-            			columns=new ArrayList<String>();
-            			columns.add(line[1]);
-            			thingsToProcess.put(line[0], columns);
-            		}
-            	}
-            }
-            csvReaderExample.processCSVWithThisOrder(thingsToProcess);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		String csvFile = csvReaderExample.WORKINGLOCATION + "\\config.csv";
+		Map<String, List<String>> thingsToProcess = new HashedMap();
+		CSVReader reader = null;
+		try {
+			reader = new CSVReader(new FileReader(csvFile), ',', '\'', 1);
+			String[] line;
+			while ((line = reader.readNext()) != null) {
+				List<String> columns;
+				if (line[4].toLowerCase().equalsIgnoreCase("yes")) {
+					if (thingsToProcess.containsKey(line[0])) {
+						thingsToProcess.get(line[0]).add(line[1]);
+					} else {
+						columns = new ArrayList<String>();
+						columns.add(line[1]);
+						thingsToProcess.put(line[0], columns);
+					}
+				}
+			}
+			csvReaderExample.processCSVWithThisOrder(thingsToProcess);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 
-
-    }
+	}
 }
